430345999e98428d16c74cb4569f818d8f71a021
adamgofast <adam@gofastcrushgoals.com>
2025-10-06 17:59:47 -0400

    Fix CORS - explicitly allow PATCH method

--- index.js
+++ index.js
@@ -24,7 +24,11 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
