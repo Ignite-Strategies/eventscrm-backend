85a6723bd282f3537a743c00cb122c1713ca2a7b
adamgofast <adam@gofastcrushgoals.com>
2025-10-08 15:24:57 -0400

    Fix CORS to allow x-admin-id header

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const PORT = process.env.PORT || 5001;
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-admin-id']
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
