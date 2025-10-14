a278a9e7bdbafa645e1cbc7c5376a6127d4fdc38
adamgofast <adam@gofastcrushgoals.com>
2025-10-14 16:42:43 -0400

    FIX: Remove VITE_ prefix from backend Google Client ID env var

--- services/personalEmailService.js
+++ services/personalEmailService.js
@@ -4,7 +4,7 @@ import { google } from 'googleapis';
 export class GmailService {
   constructor(accessToken) {
     this.oauth2Client = new google.auth.OAuth2(
-      process.env.VITE_GOOGLE_CLIENT_ID,
+      process.env.GOOGLE_CLIENT_ID,
       process.env.GOOGLE_CLIENT_SECRET,
       process.env.GOOGLE_REDIRECT_URI
     );
