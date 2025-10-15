2ffb8db857775f28afe481b4804cc7f9ee9b87f3
adamgofast <adam@gofastcrushgoals.com>
2025-10-15 18:56:49 -0400

    CRITICAL FIX: Remove goesBy from OrgMember select - moved to Contact model

--- routes/universalHydrationRoute.js
+++ routes/universalHydrationRoute.js
@@ -40,7 +40,7 @@ router.get('/', async (req, res) => {
           select: {
             id: true,
             orgId: true,
-            goesBy: true,
+            // goesBy moved to Contact model
             street: true,
             city: true,
             state: true,
